13ac1c0a4c30d85c6775c610f47ba32ae423deb7
Gustaf Borgström <gustaf.borgstrom@it.uu.se>
2020-05-13 23:54:39 +0200

    Initial placeholder code (with nonsense output)

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,60 @@
-struct GameState {
-    pub a: i32,
-    pub b: u32,
+pub enum Face {
+    UP,
+    RIGHT,
+    DOWN,
+    LEFT,
 }
 
-impl GameState {
+pub struct Tetroids {
+    tetroids: Vec<String>,
+}
+
+impl Tetroids {
     pub fn new() -> Self {
+        let mut tetroids: Vec<String> = vec![String::new(); 7];
+        tetroids[0] += "..X.";
+        tetroids[0] += "..X.";
+        tetroids[0] += "..X.";
+        tetroids[0] += "..X.";
+        tetroids[1] += ".X..";
+        tetroids[1] += ".X..";
+        tetroids[1] += ".XX.";
+        tetroids[1] += "....";
+        tetroids[2] += "..X.";
+        tetroids[2] += "..X.";
+        tetroids[2] += ".XX.";
+        tetroids[2] += "....";
+        tetroids[3] += "....";
+        tetroids[3] += ".XX.";
+        tetroids[3] += ".XX.";
+        tetroids[3] += "....";
+        tetroids[4] += ".X..";
+        tetroids[4] += ".XX.";
+        tetroids[4] += "..X.";
+        tetroids[4] += "....";
+        tetroids[5] += "..X.";
+        tetroids[5] += ".XX.";
+        tetroids[5] += ".X..";
+        tetroids[5] += "....";
+        tetroids[6] += "..X.";
+        tetroids[6] += ".XX.";
+        tetroids[6] += "..X.";
+        tetroids[6] += "....";
+
         Self {
-            a: -42,
-            b: 4242,
+            tetroids
         }
     }
 
-    pub fn get_ab(&self) -> (i32, u32) {
-        (self.a, self.b)
+    pub fn get_tetroid(&self, face: Face) -> &str {
+        match face {
+            Face::UP => self.tetroids[6].as_str(),
+            _ => ""
+        }
     }
 }
 
 fn main() {
-    let gs = GameState::new();
-    println!("{}", gs.get_ab().1);
+    let tetroids = Tetroids::new();
+    println!("{}", tetroids.get_tetroid(Face::UP));
 }
