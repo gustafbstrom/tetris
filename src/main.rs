aef0199f60cc95bec16221e9d6c1d39491ef1e30
Gustaf Borgström <gustaf.borgstrom@it.uu.se>
2020-05-23 00:12:58 +0200

    Print game board using termion

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+extern crate termion;
+
+use termion::event::Key;
+use termion::input::TermRead;
+use termion::raw::IntoRawMode;
+use std::io::{Write, stdout, stdin};
+
 pub enum Face {
     UP,
     RIGHT,
@@ -5,7 +12,23 @@ pub enum Face {
     LEFT,
 }
 
+struct GameBoard {
+    width : u32,
+    height : u32,
+
+}
+
+impl Default for GameBoard {
+    fn default() -> Self {
+        GameBoard {
+            width: 12,
+            height: 18,
+        }
+    }
+}
+
 pub struct Tetroids {
+    size: u32,
     tetroids: Vec<String>,
 }
 
@@ -42,19 +65,86 @@ impl Tetroids {
         tetroids[6] += "....";
 
         Self {
-            tetroids
+            size: 4,
+            tetroids: tetroids,
         }
     }
 
-    pub fn get_tetroid(&self, face: Face) -> &str {
+    fn rotation(&self, x: u32, y: u32, face: Face) -> u32 {
         match face {
-            Face::UP => self.tetroids[6].as_str(),
-            _ => ""
+            Face::UP => y + self.size*x,
+            Face::RIGHT => 12 + y + (self.size*x),
+            Face::DOWN => 15 - (self.size*y) - x,
+            Face::LEFT => 3 + y + (self.size*x),
         }
     }
+
+    /* TDOD: get rotated tetroid string */
+    // pub fn get_tetroid(&self, idx: u32, face: Face) -> &str {
+    //     let tetroid = String::new();
+    //     for y in 0..self.size {
+    //         for x in 0..self.size {
+    //             tetroid.push_str(tetroid[idx].get(self.rotation(x, y, face)))
+    //         }
+    //     }
+    // }
+}
+
+fn game_loop() {
+    let game_board = GameBoard {..Default::default()};
+    let tetroids = Tetroids::new();
+    println!("{}", tetroids.rotation(0, 0, Face::UP));
+    
+    let stdin = stdin();
+    let mut stdout = stdout().into_raw_mode().unwrap();
+
+    write!(stdout, "{}{}q to exit{}",
+        termion::clear::All,
+        termion::cursor::Goto(1, 1),
+        termion::cursor::Hide).unwrap();
+    stdout.flush().unwrap();
+
+    for c in stdin.keys() {
+        write!(stdout, "{}{}",
+            termion::cursor::Goto(1, 1),
+            termion::clear::CurrentLine).unwrap();
+        stdout.flush().unwrap();
+        
+        match c.unwrap() {
+            Key::Char('q') => break,
+        //     Key::Char(c) => println!("{}", c),
+        //     Key::Alt(c) => println!("Alt-{}", c),
+        //     Key::Ctrl(c) => println!("Ctrl-{}", c),
+            Key::Down => println!("<down>"),
+            Key::Up => println!("<up>"),
+            Key::Left => println!("<left>"),
+            Key::Right => println!("<right>"),
+            _ => () //println!("Other"),
+        }
+
+        // let mut line = 0u16;
+        for line in 1..game_board.height as u16 {
+            write!(stdout, "{}{}*{: <3$}*",
+                termion::cursor::Goto(1, line),
+                termion::clear::CurrentLine,
+                ' ',
+                game_board.width as usize).unwrap();    
+            // line += 1;
+        }
+        write!(stdout, "{}{}*{:*<3$}*",
+            termion::cursor::Goto(1, game_board.height as u16),
+            termion::clear::CurrentLine,
+            "",
+            game_board.width as usize).unwrap();
+        stdout.flush().unwrap();
+        // break;
+    }
+    write!(stdout, "Game over{}", termion::cursor::Show).unwrap();
 }
 
 fn main() {
-    let tetroids = Tetroids::new();
-    println!("{}", tetroids.get_tetroid(Face::UP));
+    // Take eventaul inout arguments
+
+    // Start game loop
+    game_loop();
 }
