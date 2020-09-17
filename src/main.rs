00b46ed03d6220c0c64ab0c8e1da51d0e189c45d
Gustaf Borgström <gustaf.borgstrom@it.uu.se>
2020-09-17 21:34:20 +0200

    Change flip state; refactor code
    - Move code into structs for better maintainability for when passing
      objects around.
    - Pressing 'z' changes the flip state. The tetroids are not visible yet,
      but now one can see that something changes when pressing a key.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,41 @@
-extern crate termion;
-
 use termion::event::Key;
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
 use std::io::Write;
+use rand;
 
+const TETROIDS: [[char; 16]; 7] = [
+    ['.','.','X','.',
+    '.','.','X','.',
+    '.','.','X','.',
+    '.','.','X','.',],
+    ['.','X','.','.',
+    '.','X','.','.',
+    '.','X','X','.',
+    '.','.','.','.',],
+    ['.','.','X','.',
+    '.','.','X','.',
+    '.','X','X','.',
+    '.','.','.','.',],
+    ['.','X','X','.',
+    '.','X','X','.',
+    '.','.','.','.',
+    '.','.','.','.',],
+    ['.','X','.','.',
+    '.','X','X','.',
+    '.','.','X','.',
+    '.','.','.','.',],
+    ['.','.','X','.',
+    '.','X','X','.',
+    '.','X','.','.',
+    '.','.','.','.',],
+    ['.','.','X','.',
+    '.','X','X','.',
+    '.','.','X','.',
+    '.','.','.','.',],
+];
+
+#[derive(Debug)]
 pub enum Face {
     UP,
     RIGHT,
@@ -12,57 +43,44 @@ pub enum Face {
     LEFT,
 }
 
-pub struct Tetroids {
-    size: u32,
-    tetroids: Vec<String>,
+#[derive(Debug)]
+pub struct Tetroid<'a> {
+    tetroid: &'a [char],
+    face: Face,
 }
 
-impl Tetroids {
-    pub fn new() -> Self {
-        let mut tetroids: Vec<String> = vec![String::new(); 7];
-        tetroids[0] += "..X.";
-        tetroids[0] += "..X.";
-        tetroids[0] += "..X.";
-        tetroids[0] += "..X.";
-        tetroids[1] += ".X..";
-        tetroids[1] += ".X..";
-        tetroids[1] += ".XX.";
-        tetroids[1] += "....";
-        tetroids[2] += "..X.";
-        tetroids[2] += "..X.";
-        tetroids[2] += ".XX.";
-        tetroids[2] += "....";
-        tetroids[3] += ".XX.";
-        tetroids[3] += ".XX.";
-        tetroids[3] += "....";
-        tetroids[3] += "....";
-        tetroids[4] += ".X..";
-        tetroids[4] += ".XX.";
-        tetroids[4] += "..X.";
-        tetroids[4] += "....";
-        tetroids[5] += "..X.";
-        tetroids[5] += ".XX.";
-        tetroids[5] += ".X..";
-        tetroids[5] += "....";
-        tetroids[6] += "..X.";
-        tetroids[6] += ".XX.";
-        tetroids[6] += "..X.";
-        tetroids[6] += "....";
-
-        Self {
-            size: 4,
-            tetroids: tetroids,
+impl Tetroid<'_> {
+    fn new(new_tetroid: usize) -> Self {
+        Tetroid {
+            tetroid: &TETROIDS[new_tetroid],
+            face: Face::UP,
         }
     }
+}
 
-    fn rotation(&self, x: u32, y: u32, face: Face) -> u32 {
-        match face {
-            Face::UP => y + self.size*x,
-            Face::RIGHT => 12 + y + (self.size*x),
-            Face::DOWN => 15 - (self.size*y) - x,
-            Face::LEFT => 3 + y + (self.size*x),
-        }
+fn rotation(x: u32, y: u32, face: Face) -> u32 {
+    const SIZE: u32 = 4;
+    match face {
+        Face::UP => y + SIZE*x,
+        Face::RIGHT => 12 + y + (SIZE*x),
+        Face::DOWN => 15 - (SIZE*y) - x,
+        Face::LEFT => 3 + y + (SIZE*x),
     }
+}
+
+fn check_collision(x: i32, y: i32, tet: &Tetroid, gb: &[u8]) -> bool {
+    // for py in gb.len() {
+    //     for
+    // }
+    false
+}
+
+/**
+ * Get a new random tetroid.
+ */
+fn new_tetroid() -> Tetroid<'static> {
+    let current_tetroid = rand::random::<usize>() % TETROIDS.len();
+    Tetroid::new(current_tetroid)
 }
 
 fn game_loop() {
@@ -72,11 +90,28 @@ fn game_loop() {
     let game_sprites = [' ', 'A', 'B', 'C', 'D', 'E', 'F', 'G', '=', '#'];
     let mut game_field = [0u8; FIELD_WIDTH*FIELD_HEIGHT];
 
+    let mut current_tetroid = new_tetroid();
+    let mut x = FIELD_HEIGHT as i32;
+    let mut y = 0i32;
+
     let mut stdin = termion::async_stdin().keys();
     let mut stdout = std::io::stdout().into_raw_mode().unwrap();
 
     let tick_size = std::time::Duration::from_millis(50);
     
+    /* Init board */
+    for py in 0..FIELD_HEIGHT {
+        for px in 0..FIELD_WIDTH {
+            let idx = py*FIELD_WIDTH + px;
+            if px == 0 || px == FIELD_WIDTH-1 || py == FIELD_HEIGHT-1 {
+                game_field[idx] = 9;
+            }
+            else {
+                game_field[idx] = 0;
+            }
+        }
+    }
+
     /* PRE-RENDER, CLEAN */
     write!(stdout, "{}{}{}",
         termion::clear::All,
@@ -95,32 +130,39 @@ fn game_loop() {
                 Key::Char('q') => break 'game_loop,
                 Key::Down => {
                     /* TODO: Move down */
+                    let is_collision = check_collision(x, y+1, &current_tetroid, &game_field);
+                    if !is_collision {
+                        y += 1;
+                    }
                 },
                 Key::Left => {
                     /* TODO: Move left */
+                    let is_collision = check_collision(x-1, y, &current_tetroid, &game_field);
+                    if !is_collision {
+                        x -= 1;
+                    }
                 },
                 Key::Right => {
                     /* TODO: Move right */
+                    let is_collision = check_collision(x+1, y, &current_tetroid, &game_field);
+                    if !is_collision {
+                        x += 1;
+                    }
                 },
                 Key::Char('z') => {
-                    /* TODO: Rotate tetroid */
+                    match current_tetroid.face {
+                        Face::UP => current_tetroid.face = Face::RIGHT,
+                        Face::RIGHT => current_tetroid.face = Face::DOWN,
+                        Face::DOWN => current_tetroid.face = Face::LEFT,
+                        Face::LEFT => current_tetroid.face = Face::UP,
+                    }
                 },
                 _ => (),
             }
         }
 
         /********** UPDATE GAME ENGINE STATE **********/
-        for py in 0..FIELD_HEIGHT {
-            for px in 0..FIELD_WIDTH {
-                let idx = py*FIELD_WIDTH + px;
-                if px == 0 || px == FIELD_WIDTH-1 || py == FIELD_HEIGHT-1 {
-                    game_field[idx] = 9;
-                }
-                else {
-                    game_field[idx] = 0;
-                }
-            }
-        }
+
 
         /********** RENDER GAME BOARD **********/
         write!(stdout, "{}", termion::cursor::Goto(1, 1));
@@ -133,6 +175,7 @@ fn game_loop() {
             write!(stdout, "{}", termion::cursor::Goto(1, py as u16+2));
         }
         stdout.flush().unwrap();
+        write!(stdout, "{:?}", current_tetroid.face);
     }
     write!(stdout, "Game over{}", termion::cursor::Show).unwrap();
 }
